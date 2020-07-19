a1ff495bc3c05b3c6d2531f75edb4f7a29c28555
Restyled.io <commits@restyled.io>
2020-07-19 04:54:01 +0000

    Restyled by clang-format

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,39 +17,33 @@ flex mb-2 space-x-3`
 
 const Home = () => (
   <div
-    style={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      flexDirection: "column",
-    }}
-  >
-    {" "}
-    <SEO title="Home" />
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100%",
-        height: "100%",
-      }}
-    >
-      {" "}
-      <div style={{ flex: 1 }}>
-        <Image />
-      </div>
+style = {
+  {
+    display: "flex", justifyContent: "center", alignItems: "center",
+        flexDirection: "column",
+  }
+} > {" "}<SEO title = "Home" />< div
+style =
+    {
+      {
+        display: "flex", justifyContent: "center", alignItems: "center",
+            width: "100%", height: "100%",
+      }
+    } > {" "}<div style = {
+      { flex: 1 }
+    }><Image />
+    </div>
       <div style={{ flex: 2, marginLeft: "5rem", marginRight: "5rem" }}>
         <Name>Hello, I'm Sudhanshu.</Name>
-        <Para>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit
-          in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa
-          qui officia deserunt mollit anim id est laborum.
-        </Para>
+    <Para>Lorem ipsum dolor sit amet,
+    consectetur adipiscing elit,
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad
+minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in
+    voluptate velit esse cillum dolore eu fugiat nulla
+pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa
+qui officia deserunt mollit anim id est laborum.<
+    /Para>
 
         <Para>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -61,17 +55,16 @@ const Home = () => (
           culpa qui officia deserunt mollit anim id est laborum.
         </Para>
 
-        <Para>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit
-          in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa
-          qui officia deserunt mollit anim id est laborum.
-        </Para>
-      </div>
-    </div>
+    <Para>Lorem ipsum dolor sit amet,
+    consectetur adipiscing elit,
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad
+minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in
+    voluptate velit esse cillum dolore eu fugiat nulla
+pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa
+qui officia deserunt mollit anim id est laborum.</Para>
+      </div><
+    /div>
     <div
       style={{
         display: "flex",
@@ -83,25 +76,22 @@ const Home = () => (
       <SocialIcons>
         <a
           href="https:/ /
-            linkedin
-                .com / in /perfectsudh"
+        linkedin
+            .com / in /perfectsudh"
           target="_blank"
           rel="noopener"
         >
-          <Icons name="linkedin" />
-        </a>
+          <Icons name="linkedin" /><
+    /a>
 
         <a
-          href="https:/ / github.com /
-                     tsuki42 "
-          target="_blank "
-          rel=" noopener "
-        >
-          <Icons name="github" />
-        </a>
+          href="https:/ / github.com / tsuki42 "
+target = "_blank "
+rel = " noopener " > <Icons name = "github" /><
+      /a>
         <a
           href="https:/ / twitter.com / __tsuki__42 "
-          target="_blank"
+target = "_blank"
           rel="noopener"
         >
           <Icons name="twitter" />
@@ -111,4 +101,4 @@ const Home = () => (
   </div>
 )
 
-export default Home
+          export default Home
